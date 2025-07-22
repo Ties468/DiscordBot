2f0bb85a77b9d60f80ef89b061aac369a52b0f4d
Tie <luciano00luci@gmail.com>
2025-07-22 20:56:31 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+Token = 'Your_Token'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents) 
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} - {bot.user.id}')
+
+@bot.command()
+async def join(ctx)
+    
+bot.run(Token) # Launcher
